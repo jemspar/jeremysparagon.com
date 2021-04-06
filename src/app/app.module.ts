d3a809fe2bdb61949bf6734c63b49b9d4a2acdd6
Jeremy Sparagon <j.a.sparagon@outlook.com>
2021-04-06 11:23:43 -0700

    project component initialized

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,24 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserModule, Title } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProjectComponent } from './project/project.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProjectComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+  	Title
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/projects.ts
@@ -0,0 +1,20 @@
+export const projects = [
+  {
+    id: 0,
+    title: "Keegan Construction",
+    url: "https://keeganconstruction.com/",
+    onHome: true,
+  },
+  {
+    id: 1,
+    title: "Rootstalk",
+    url: "https://rootstalk.grinnell.edu",
+    onHome: true,
+  },
+  {
+    id: 2,
+    title: "Terseness",
+    url: "./terseness/terseness.html",
+    onHome: true,
+  }
+]
